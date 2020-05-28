ef132ce24f142ed60148d612e331580bf8b0597f
Carter Campbell <campbellcarter444@gmail.com>
2020-05-28 17:51:11 -0600

    Door opens to 90 degrees smoothly upon game start

--- Source/BuildingEscape2/OpenDoor.cpp
+++ Source/BuildingEscape2/OpenDoor.cpp
@@ -20,13 +20,6 @@ void UOpenDoor::BeginPlay()
 {
 	Super::BeginPlay();
 
-	float MyFloat = 90.f;
-
-	FRotator CurrentRotation = GetOwner()->GetActorRotation();
-	FRotator NewRotation = CurrentRotation;
-	NewRotation.Yaw += 90.f;
-
-	GetOwner()->SetActorRotation(NewRotation);
 	
 }
 
@@ -36,6 +29,13 @@ void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompon
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	// ...
+	UE_LOG(LogTemp, Warning, TEXT("%s"), *GetOwner()->GetActorRotation().ToString());
+	UE_LOG(LogTemp, Warning, TEXT("Yaw is: %f"), GetOwner()->GetActorRotation().Yaw);
+
+	float CurrentYaw = GetOwner()->GetActorRotation().Yaw;
+	FRotator OpenDoor(0.f, TargetYaw, 0.f);
+	OpenDoor.Yaw = FMath::Lerp(CurrentYaw, TargetYaw, 0.02f);
+
+	GetOwner()->SetActorRotation(OpenDoor);
 }
 
